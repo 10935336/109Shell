d64dc6eeff9e145de4a45923142798898c96974c
10935336 <a10935336@live.com>
2024-02-29 18:41:14 +0800

    deprecated

--- update_github_hosts_from_next-hosts.sh
+++ update_github_hosts_from_next-hosts.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+
+#此脚本已弃用，请使用： https://github.com/10935336/109Shell/blob/main/update_github_hosts.sh
+#This script is deprecated, please use: https://github.com/10935336/109Shell/blob/main/update_github_hosts.sh
+
+
 #更新 Github Host - 从 https://github.com/ineo6/hosts 获取最新的 Github Host 并更新，有助于改善中国大陆的 Github 网络封锁。
 #Update Github Host - Get the latest Github Host from https://github.com/ineo6/hosts and update it, which will help improve the Github network blockade in mainland China.
 #Author: 10935336
 #Creation date: 2023-02-03
-#Modified date: 2023-03-22
+#Modified date: 2023-04-29
 
 
 #### Variable #####
@@ -41,8 +47,8 @@ sed -i '1,/# GitHub Host Start/{/# GitHub Host Start/!d;}' next-hosts
 # 将新的 hosts 内容添加到现有 hosts 暂存中
 cat next-hosts >> hosts-temp
 
-# 删除现有 hosts 暂存中可能多余的内容
-sed -i '/# New！欢迎使用基于DNS的新方案/,/# 也可以关注公众号：湖中剑，保证不迷路/d' hosts-temp
+# 删除现有 hosts 暂存中多余的内容
+sed -i '/# 地址可能会变动，请务必关注GitHub、Gitlab获取最新消息/,/# 也可以关注公众号：湖中剑，保证不迷路/d' hosts-temp
 
 # 将更新后的 hosts 内容写入 /etc/hosts
 cat hosts-temp > /etc/hosts
